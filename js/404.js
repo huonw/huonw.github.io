03661e0090f42ba02c16736558f776232b537fb7
huonw <huonw@users.noreply.github.com>
2025-09-07 23:22:03 +0000

    deploy: 92619eaa9a8df11722d894b096e45c2fc9611bef

--- js/404.js
+++ js/404.js
@@ -45,6 +45,8 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     };
     var posts = [
         
+        {path: "/blog/2025/09/deprecating-vs-removing/", title: "Is that a deprecation? Or is it just removed?"},
+        
         {path: "/blog/2025/08/copy-as-curl/", title: "Convenient 'Copy as cURL': explicit, executable, editable request replays"},
         
         {path: "/blog/2025/04/standalone-shell-script-reproducer/", title: "Communicating bugs: use a single standalone shell script"},
